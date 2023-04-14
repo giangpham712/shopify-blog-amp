c12e39b53f493f9d2de80f2625874f8fdd5d70d3
Giang Pham <giang.phamhoang712@gmail.com>
2023-04-15 01:54:13 +1000

    Fix use of amp-script

--- public/static/wknd.js
+++ public/static/wknd.js
@@ -1,7 +1,7 @@
 (function(d) {
   console.log('Loading WKND script');
   var e = d.createElement('script');
-  e.src = d.location.protocol + '//tag.wknd.ai/3607/i.js';
+  e.src = 'https://tag.wknd.ai/3607/i.js';
   e.async = true;
   d.getElementsByTagName("head")[0].appendChild(e);
 }(document));
