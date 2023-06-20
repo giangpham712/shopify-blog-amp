8b8bc0636354ab8567c6b088a2ff80dd86529b50
Giang Pham <giang.phamhoang712@gmail.com>
2023-06-20 23:04:31 +1000

    Add klaviyo popup script

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
           <script async="async" custom-element="amp-skimlinks"
                   src="https://cdn.ampproject.org/v0/amp-skimlinks-0.1.js" class="jsx-3778485153"></script>
           <script async custom-element="amp-script" src="https://cdn.ampproject.org/v0/amp-script-0.1.js"></script>
+          <script async custom-element="amp-script" src="https://cdn.shopify.com/s/files/1/0648/1955/files/popup-script.js?v=1687194598"></script>
           <meta name="amp-script-src" content="sha384-bPBkuW4E1H49KTCaFi9FtTqb_decMdd2zpPocqe4siq12h1y50f7a535BczIhjga" />
         </Head>
         <body>
