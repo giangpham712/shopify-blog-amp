1aeaaab90659e2643aa48e7ed570c2e672724463
Giang Pham <giang.phamhoang712@gmail.com>
2023-07-13 21:26:48 +1000

    Remove amp-script extension

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,6 @@ class MyDocument extends Document {
         <Head>
           <script async="async" custom-element="amp-skimlinks"
                   src="https://cdn.ampproject.org/v0/amp-skimlinks-0.1.js" class="jsx-3778485153"></script>
-          <script async custom-element="amp-script" src="https://cdn.ampproject.org/v0/amp-script-0.1.js"></script>
         </Head>
         <body>
         <amp-skimlinks layout="nodisplay" publisher-code="158246X1624815"></amp-skimlinks>
